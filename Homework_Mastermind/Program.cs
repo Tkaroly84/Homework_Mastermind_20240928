710588431d2ced6bfde2a1984ee17735e11d2b1f
tothk <tothk@ZENBOOK>
2024-09-28 08:33:41 +0200

    Add project files.

---
+++ Homework_Mastermind/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Homework_Mastermind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üdvözöllek a Barkóba játékomban!\n\n" +
+                              "Próbáld meg kitalálni a lehető legkevesebb próbálkozással milyen számra gondoltam...");
+
+
+
+
+//deklaráció
+            int tipp;
+            int tippekSzama = 0;
+            bool sikeresSzambekeres;
+            Random random = new Random();
+            int gondoltSzam = random.Next(1, 1001);
+
+
+
+
+//szám bekérése és TryParse
+            do
+            {
+                Console.WriteLine("Kérem a tipped:");
+                string input = Console.ReadLine();
+                sikeresSzambekeres = int.TryParse(input, out tipp);
+
+                if (!sikeresSzambekeres)
+                {
+                    Console.WriteLine("Kérlek számot adj meg!");
+                }
+            }
+            while (!sikeresSzambekeres);
+
+
+
+
+//a tipp és a gondolt szám kiértékelése
+            if (gondoltSzam > tipp)
+            {
+                Console.WriteLine("Nagyobb számra gondoltam!");
+            }
+            else if (gondoltSzam < tipp)
+            {
+                Console.WriteLine("Kisebb számra gondoltam!");
+            }
+            else
+            {
+                Console.WriteLine($"Gratulálok!\n\nA tippjed száma: {tippekSzama}");
+            }
+        }
+    }
+}
