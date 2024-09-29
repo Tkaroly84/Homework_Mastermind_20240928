8e8c1ddb60cc9f45e033396bc2774c1ea80d7957
Károly Tóth <toth.karolycar84@gmail.com>
2024-09-29 11:39:46 +0200

    Update refactoring

--- Homework_Mastermind/Program.cs
+++ Homework_Mastermind/Program.cs
@@ -1,39 +1,56 @@
-﻿namespace Homework_Mastermind
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Homework_Mastermind
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Udvozlo();
+            Kiertekeles();
+        }
+        #region Üdvözlő
+        public static void Udvozlo()
+        {
             Console.WriteLine("Üdvözöllek a Barkóba játékomban!\n\n" +
-                              "Próbáld meg kitalálni a lehető legkevesebb próbálkozással milyen számra gondoltam...");
-
-            // Deklaráció
+              "Próbáld meg kitalálni a lehető legkevesebb próbálkozással milyen számra gondoltam...");
+        }
+        #endregion
+        #region Szám bekérése és TryParse
+        public static int SzamBekeres()
+        {
             int tipp;
-            int tippekSzama = 0;
             bool sikeresSzambekeres;
+
+            do
+            {
+                Console.WriteLine("Kérem a tipped:");
+                string input = Console.ReadLine();
+                sikeresSzambekeres = int.TryParse(input, out tipp);
+
+                if (!sikeresSzambekeres)
+                {
+                    Console.WriteLine("Kérlek számot adj meg!");
+                }
+            }
+            while (!sikeresSzambekeres);
+
+            return tipp;
+        }
+        #endregion
+        #region A tipp és a gondolt szám kiértékelése
+        public static void Kiertekeles()
+        {
             Random random = new Random();
             int gondoltSzam = random.Next(1, 1001);
+            int tippekSzama = 0;
             bool talalat = false;
 
             while (!talalat)
             {
-                // Szám bekérése és TryParse
-                do
-                {
-                    Console.WriteLine("Kérem a tipped:");
-                    string input = Console.ReadLine();
-                    sikeresSzambekeres = int.TryParse(input, out tipp);
-
-                    if (!sikeresSzambekeres)
-                    {
-                        Console.WriteLine("Kérlek számot adj meg!");
-                    }
-                }
-                while (!sikeresSzambekeres);
-
+                int tipp = SzamBekeres();
                 tippekSzama++;
 
-                // A tipp és a gondolt szám kiértékelése
                 if (gondoltSzam > tipp)
                 {
                     Console.WriteLine("Nagyobb számra gondoltam!");
@@ -48,6 +65,8 @@
                     talalat = true; // A ciklus befejezéséhez
                 }
             }
+
         }
+        #endregion
     }
 }
